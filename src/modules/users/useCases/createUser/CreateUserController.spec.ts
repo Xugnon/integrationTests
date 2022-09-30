688396b8f384ee73afc016293953766c6fd34005
Lucas <lucas>
2022-09-30 14:45:48 -0300

    FirstCommit

---
+++ src/modules/users/useCases/createUser/CreateUserController.spec.ts
@@ -0,0 +1,26 @@
+import { Connection, createConnection } from "typeorm";
+import request from "supertest";
+import { app } from "../../../../app";
+
+let connection: Connection;
+describe("Create User Controller", () => {
+  beforeAll(async () => {
+    connection = await createConnection();
+    await connection.runMigrations();
+  });
+
+  afterAll(async () => {
+    await connection.dropDatabase();
+    await connection.close();
+  });
+
+  it("should be able to create a new user", async () => {
+    const response = await request(app).post("/api/v1/users").send({
+      name: "testUser",
+      email: "testUser@example.com",
+      password: "testPassword",
+    });
+
+    expect(response.statusCode).toBe(201);
+  })
+})
